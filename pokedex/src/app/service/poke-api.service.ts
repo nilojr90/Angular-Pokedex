cdf4f1eef54ea6ac550543317475ec230056d394
Nilson Ramos de Menezes Júnior <nilojr90@gmail.com>
2021-01-15 12:40:36 -0300

    criação metodos getPokemonById getPokemonList

--- pokedex/src/app/service/poke-api.service.ts
+++ pokedex/src/app/service/poke-api.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
+import { IPokedexEntry } from '../model/pokedex';
+import { IPokemon } from '../model/pokemon';
 import { ditto } from './ditto';
+import { pokemonList } from './pokemon-list';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +12,15 @@ export class PokeApiService {
 
   constructor() { }
 
-  getPokemonByName(name: string) {
+  getPokemonByName(name: string): IPokemon {
     return ditto;
+  }
+
+  getPokemonById(id: number): IPokemon {
+    return ditto;
+  }
+
+  getPokemonList(limit: number, offset: number): IPokedexEntry[] {
+    return pokemonList.results;
   }
 }
